377f776c5b6c45386559c3a7caf39ca7611166c3
Aronex-alive <arya120609@gmail.com>
2025-09-15 12:34:25 +0530

    file

---
+++ OTP Generation.py
@@ -0,0 +1,31 @@
+'''#WAP a program for generating a OTP which should contain 6 numbers
+from random import randint
+for i in range(1,7):
+    a=randint(0,9)
+    print(a)
+print("next otp is")
+otp=1
+while otp<=6:
+    x=randint(0,9)
+    print(x)
+    otp+=1'''
+
+
+
+from random import randint
+
+# First OTP
+otp = ""
+for i in range(1,6):
+    otp += str(randint(0, 9))
+print("Your OTP is:", otp)
+
+# Second OTP
+otp = ""
+for i in range(6):
+    otp += str(randint(0, 9))
+print("Your OTP is:", otp)
+
+
+
+    
